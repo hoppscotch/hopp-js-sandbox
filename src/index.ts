f20887f8f5e83bf820deb0368a93afc3184e771b
Andrew Bastin <andrewbastin.k@gmail.com>
2021-07-04 16:01:39 -0400

    Better typings

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,12 @@
 import { execTestScript, TestDescriptor } from "./test-runner"
 
+/**
+ * Executes a given test script on the test-runner sandbox
+ * @param testScript The string of the script to run
+ * @returns A promise to a TestDescriptor with the final status
+ */
 export async function runTestScript(
   testScript: string
 ): Promise<TestDescriptor> {
   return (await execTestScript(testScript))[0]
 }
-
-// eslint-disable-next-line prettier/prettier
-(async () => {
-  console.dir(
-    await runTestScript(
-      `
-    pw.test("Arithmetic operations and toBe", () => {
-      const size = 500 + 500;
-      pw.expect(size).toBe(1000);
-      pw.expect(size - 500).toBe(500);
-      pw.expect(size * 4).toBe(4000);
-      pw.expect(size / 4).toBe(250);
-    });
-    pw.test("toBeLevelxxx", () => {
-      pw.expect(200).toBeLevel2xx();
-      pw.expect(204).toBeLevel2xx();
-      pw.expect(300).not.toBeLevel2xx();
-      pw.expect(300).toBeLevel3xx();
-      pw.expect(304).toBeLevel3xx();
-      pw.expect(204).not.toBeLevel3xx();
-      pw.expect(401).toBeLevel4xx();
-      pw.expect(404).toBeLevel4xx();
-      pw.expect(204).not.toBeLevel4xx();
-      pw.expect(501).toBeLevel5xx();
-      pw.expect(504).toBeLevel5xx();
-      pw.expect(204).not.toBeLevel5xx();
-    });
-    pw.test("toBeType", () => {
-      pw.expect("hello").toBeType("string");
-      pw.expect(10).toBeType("number");
-      pw.expect(true).toBeType("boolean");
-      pw.expect("deffonotanumber").not.toBeType("number");
-    });
-    pw.test("toHaveLength", () => {
-      const arr = [1, 2, 3];
-      pw.expect(arr).toHaveLength(3);
-      pw.expect(arr).not.toHaveLength(4);
-    });
-    `
-    ),
-    {
-      depth: 100,
-    }
-  )
-})()
